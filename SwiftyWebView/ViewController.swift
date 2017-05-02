490a9e706643f79980a16bd23b56d4215d3da0c4
sugamkalra <skappirio>
2017-05-02 19:57:45 +0530

    Handling UIWebView Delegates

--- SwiftyWebView/ViewController.swift
+++ SwiftyWebView/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIWebViewDelegate {
 
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
